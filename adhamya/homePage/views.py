fa8384883faa20ad5223515fad38ebfb12a3dfa3
theganeshsharma <44599755+theganeshsharma@users.noreply.github.com>
2018-10-30 18:29:32 +0530

    Updated Views.py
    Added registration views

--- adhamya/homePage/views.py
+++ adhamya/homePage/views.py
@@ -1,33 +1,74 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from homePage.forms import formLogin,formFeedback
-from .models import Feedback,User
+from django.contrib.auth import authenticate,login,logout
+
+from .models import *
 
 
 #--home Page--
 def index(request):
-    return render(request,'homePage/index.html',{'context': ''})
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            user = request.user
+            context = {
+                'member': Member.objects.filter(user=user),
 
-#--feedback form--
-def feedback(request):
-    forms = formFeedback()
-    if(request.method == "POST"):
-        feedbackObj = Feedback()
-        if forms.is_valid():
-            return HttpResponse('Form is invalid')
+            }
+            return render(request, 'homePage/index.html', context)
         else:
-            forms = formFeedback(request.POST)
-            feedbackObj.Name = request.POST.get('name')
-            feedbackObj.EmailID = request.POST.get('email')
-            feedbackObj.Comment = request.POST.get('comments')
-            feedbackObj.save()
-            return render(request, 'homePage/index.html', {'forms': forms})
-    else:
-        return HttpResponse('Form is invalid')
+            return render(request,'homePage/index.html')
 
-#--login Page--
-def login(request):
-    return render(request, 'homePage/login.html', {'context': ''})
+    if request.method == "POST":
+        return redirect('homePage:index')
+
+
+
+
+#--register--
+
+def registration(request):
+    if request.method == "GET":
+        return render(request,'homePage/register.html')
+
+    if request.method == "POST":
+        ufname=request.POST.get("reg-fname",None)
+        ulname=request.POST.get("reg-lname",None)
+        uID =request.POST.get("reg-uid",None)
+        uemail = request.POST.get("reg-email",None)
+        upass = request.POST.get("reg-pass",None)
+        uphone = request.POST.get("reg-phone",None)
+        ucat = request.POST.get("reg-cat",None)
+        new_user = User.objects.create(first_name=ufname,last_name=ulname,username=uID,email=uemail,password=upass)
+        new_user.set_password(upass)
+        new_mem = Member.objects.create(user=new_user,MobileNo=uphone,category=ucat)
+
+        context={
+            'member':new_mem,
+        }
+
+        return render(request,'homePage/login.html',context)
+
+
+
+
+#--login--
+def user_login(request):
+    if request.method=="GET":
+        return render(request, 'homePage/register.html')
+    if request.method=="POST":
+        usermail = request.POST.get("login-email",None)
+        user = Member.objects.get(user__email=usermail)
+        username = user.user.username
+        password = request.POST.get("login-password",None)
+
+        guest = authenticate(username=username,password=password)
+        if guest is not None:
+            if guest.is_active:
+                login(request, guest)
+                return redirect('homePage:index')
+
+        return render(request, 'homePage/login.html', context={'error': "Invalid Credentials"})
+
 
 
 
